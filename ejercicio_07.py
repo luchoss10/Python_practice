416dfc21fbbb980f3599bea46c24e8648070d607
Lucho <lucncd@gmail.com>
2021-08-06 09:05:21 -0500

    Modificacion ejercicio 07

--- ejercicio_07.py
+++ ejercicio_07.py
@@ -1,1 +1,23 @@
-print('Hola mundo')
+"""
+Ejercicio 07
+
+Calcula el numero de digitos del numero entero Ingresado
+
+"""
+
+def num_digitos(num):
+    if num < 0:
+        num = num*(-1)
+    
+    i = 0
+
+    while num >= 10**i:
+        i +=1
+    
+    return(i)
+
+#if __name__ == '__main__':
+
+i = num_digitos(int(input('Ingrese un numero entero: ')))
+
+print('El numero de digitos es: {}'.format(i))   
